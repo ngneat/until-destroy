b994c36c2e69746085a6a8f0c8c3090417c7c24c
Netanel Basal <netanel7799@gmail.com>
2018-08-20 08:32:29 +0300

    feat(core): remove decorator and support for older rx versions

--- dist/umd/index.js
+++ dist/umd/index.js
@@ -10,7 +10,6 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var take_until_destroy_1 = require("./take-until-destroy");
-    exports.TakeUntilDestroy = take_until_destroy_1.TakeUntilDestroy;
     exports.untilDestroyed = take_until_destroy_1.untilDestroyed;
 });
 //# sourceMappingURL=index.js.map
