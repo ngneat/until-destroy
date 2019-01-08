08c48c69072c36630422a929c7416e1d1c9d1bfd
Alex Malkevich <malkevich.alex@gmail.com>
2019-01-08 16:39:24 +0100

    feat(decorator): add @AutoUnsubscribe property decorator
    Allows to apply `untilDestroyed` operator on pipe properties via decorator

---
+++ src/decorator.ts
@@ -0,0 +1,34 @@
+import { untilDestroyed } from './take-until-destroy';
+
+/**
+ * Autamatically unsubscribes from pipe when component destroyed.
+ *
+ * @example
+ * ```ts
+ * @Component({...})
+ * class MyComponent implements OnDestroy {
+ *   @AutoUnsubscribe()
+ *   stream$ = new Observable(...);
+ *
+ *   // OnDestroy method is required by Angular Compiler
+ *   ngOnDestroy() {}
+ * }
+ * ```
+ *
+ * Uses {@link untilDestroyed} operator on the pipe.
+ *
+ * Do not forget to implement {@link OnDestroy} life-cycle hook.
+ */
+export function AutoUnsubscribe(destroyMethodName?: string): PropertyDecorator {
+  return (target, propKey) => {
+    const originalProp = target[propKey];
+
+    if (delete target[propKey]) {
+      Object.defineProperty(target, propKey, {
+        enumerable: true,
+        configurable: true,
+        value: originalProp.pipe(untilDestroyed(target)),
+      });
+    }
+  };
+}
